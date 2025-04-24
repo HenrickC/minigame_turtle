a15e1fde37bcfa624a82e9c9b85fd8043035902d
HenrickC <106536163+HenrickC@users.noreply.github.com>
2025-04-24 18:04:42 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,23 @@ t.speed(1) #ddefine velocidade
 
 
 
+
+def escolha_rotacao():
+    print("Rotacionar para d:direita, e:esquerda ou n:não rotacionar \n")
+    resp = input()
+    return resp
+
+def rotacao_direita(turtle):
+    print("Quanto para a direita devemos rotacionar? \n")
+    graus = int(input())
+    t.right(graus)
+
+def rotacao_esquerda(turtle):
+    print("Quanto para a esquerda devemos rotacionar? \n")
+    graus = int(input())
+    t.left(graus)
+
+
 while True:
     print("Para onde devemos ir? \"f:frente\" ou \"t:trás\"\n")
     direcao = input();
@@ -17,20 +34,16 @@ while True:
         move = int(input())
         t.backward(move)
 
-    print("Rotacionar para d:direita, e:esquerda ou n:não rotacionar \n")
-    resp = input()
+    resp = escolha_rotacao()
     if(resp == 'd'):
-        print("Quanto para a direita devemos rotacionar? \n")
-        graus = int(input())
-        t.right(graus)
+        rotacao_direita(t)
     elif(resp == 'e'):
-        print("Quanto para a esquerda devemos rotacionar? \n")
-        graus = int(input())
-        t.left(graus)
+       rotacao_esquerda(t)
     else:
         continue
     print("Continuar andando?\n")
     resp2 = input()
     if(resp2 == 'n'):
         break
+
 input()
